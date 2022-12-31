1ef0e166e6e83bfceeea4f68b58d2a79a0625055
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2022-12-31 12:47:26 +0530

    deployment settings changes and updates

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require("path");
-const helmet = require("helmet");
-const compression = require("compression");
 const morgan = require("morgan");
 
 const app = express();
